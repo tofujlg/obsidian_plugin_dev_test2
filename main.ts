1d4453da76f73faa74c30f1f60d9c8ea0c46532c
奥村　耕平 <okumura_kouhei@gridworld.co>
2022-10-08 13:58:27 +0900

    Clean main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-
 export default class MyPlugin extends Plugin {
 	async onload() {
 			new Notice('This is a nice Cnotice!');
