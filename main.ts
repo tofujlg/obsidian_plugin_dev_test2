7c3d7e16267bd96bbf4acee5ac50419c77efb8cf
奥村　耕平 <okumura_kouhei@gridworld.co>
2022-10-13 00:01:56 +0900

    Add Ribon

--- main.ts
+++ main.ts
@@ -2,7 +2,10 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 export default class MyPlugin extends Plugin {
 	async onload() {
-			new Notice('This is a nice Cnotice!');
+			new Notice('Test Plugin Loaded!');
+			this.addRibbonIcon("eye","Test Plugin",()=>{
+				new Notice('You Clicked button!')
+			})
 		};
 
 	onunload() {
